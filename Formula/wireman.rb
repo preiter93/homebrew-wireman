6946ed4f900cd3e813ede470d99baa317b20e832
Philipp Reiter <p.reiter93@gmail.con>
2024-10-08 00:10:36 +0200

    Simplify

--- Formula/wireman.rb
+++ Formula/wireman.rb
@@ -7,29 +7,29 @@ class Wireman < Formula
   head "https://github.com/preiter93/wireman.git", branch: "main"
   version "0.1.4"
 
-  depends_on "rust" => :build
+  # depends_on "rust" => :build
 
-  bottle do
-    root_url "https://github.com/preiter93/wireman/releases/download/v0.1.4"
-    # macOS Apple Silicon (aarch64)
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "577604876f2ba9d24b6f6fa42bf5bc50b0cb20843d143a185d6630f805f37de6"
-    # macOS Intel (x86_64)
-    sha256 cellar: :any_skip_relocation, monterey: "e28f9d45d0efb46d3b901244a89fd37215cdbfe6bc3bad7b0b3efd3d59f4bc09"
-    # Linux ARM (aarch64)
-    sha256 cellar: :any_skip_relocation, linux_arm64: "0d11f756e57a9a5d2ea15b355796ec47e50b048548f6fc57bc96cbf6d2c86da1"
-    # Linux Intel 32-bit (i686)
-    sha256 cellar: :any_skip_relocation, linux_i686: "5bac1b8c324ee865f7391c7c209321755a7f79615aa901d4207220da5f7d80c5"
-    # Linux Intel 64-bit (x86_64)
-    sha256 cellar: :any_skip_relocation, linux_x86_64: "eb02fd2b9053c4c6a1c4de0657a9e733644cd2c331ebc1da0d2c8d291b482737"
-  end
+  # bottle do
+  #   root_url "https://github.com/preiter93/wireman/releases/download/v0.1.4"
+  #   # macOS Apple Silicon (aarch64)
+  #   sha256 cellar: :any_skip_relocation, arm64_monterey: "577604876f2ba9d24b6f6fa42bf5bc50b0cb20843d143a185d6630f805f37de6"
+  #   # macOS Intel (x86_64)
+  #   sha256 cellar: :any_skip_relocation, monterey: "e28f9d45d0efb46d3b901244a89fd37215cdbfe6bc3bad7b0b3efd3d59f4bc09"
+  #   # Linux ARM (aarch64)
+  #   sha256 cellar: :any_skip_relocation, linux_arm64: "0d11f756e57a9a5d2ea15b355796ec47e50b048548f6fc57bc96cbf6d2c86da1"
+  #   # Linux Intel 32-bit (i686)
+  #   sha256 cellar: :any_skip_relocation, linux_i686: "5bac1b8c324ee865f7391c7c209321755a7f79615aa901d4207220da5f7d80c5"
+  #   # Linux Intel 64-bit (x86_64)
+  #   sha256 cellar: :any_skip_relocation, linux_x86_64: "eb02fd2b9053c4c6a1c4de0657a9e733644cd2c331ebc1da0d2c8d291b482737"
+  # end
 
   # def install
   #   bin.install "wireman"
   # end
-  def install
-    # We pass this arg to disable the `dynamic-analysis` feature on macOS.
-    # This feature is not supported on macOS and fails to compile.
+  
+  depends_on "rust" => :build
 
+  def install
     system "cargo", "install", *std_cargo_args
   end
 
