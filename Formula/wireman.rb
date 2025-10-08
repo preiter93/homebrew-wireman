a9110e9434f50d3a4924b1757066b42e955e23a3
Philipp Reiter <p.reiter93@gmail.con>
2025-10-08 19:59:12 +0200

    publish v0.2.10

--- Formula/wireman.rb
+++ Formula/wireman.rb
@@ -1,27 +1,27 @@
 class Wireman < Formula
   desc "Wireman: A grpc client for the terminal"
   homepage "https://github.com/preiter93/wireman"
-  version "0.2.9"
+  version "0.2.10"
   license "MIT"
   head "https://github.com/preiter93/wireman.git", branch: "main"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/preiter93/wireman/releases/download/v0.2.9/wireman-0.2.9-aarch64-apple-darwin.tar.gz"
-      sha256 "3a5885ec346de37ba79fb090abaaa80c010d90df13d3d878afe946e9cb9e0b8c"
+      url "https://github.com/preiter93/wireman/releases/download/v0.2.10/wireman-0.2.10-aarch64-apple-darwin.tar.gz"
+      sha256 "57a3cd1d38b4822cba30222fec9b62fc51f097d7c732032367bf9e319c89e9b2"
     else
-      url "https://github.com/preiter93/wireman/releases/download/v0.2.9/wireman-0.2.9-x86_64-apple-darwin.tar.gz"
-      sha256 "c968beb7b23e044c3ff8d5ff986de52ab75ef7fb91a7b5ad719a684ab4ec8683"
+      url "https://github.com/preiter93/wireman/releases/download/v0.2.10/wireman-0.2.10-x86_64-apple-darwin.tar.gz"
+      sha256 "924dfdc8cb331c82b9b71967e873dd8436bd1eb1637d78cf48d416f032ba7925"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/preiter93/wireman/releases/download/v0.2.9/wireman-0.2.9-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "0809e6c6ea8e07f96598de1ce7493ced50c3944f7002ff26e02adc4dbd6405b4"
+      url "https://github.com/preiter93/wireman/releases/download/v0.2.10/wireman-0.2.10-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "62f30b9190e1e811de4ea1c07fe872dfed8ece64048327dbf2edb2b3205cce4a"
     else
-      url "https://github.com/preiter93/wireman/releases/download/v0.2.9/wireman-0.2.9-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "cd715458782e1deb446a0ed8d70af10dec8c0c7bdbd8a37c923e4735ed506d3d"
+      url "https://github.com/preiter93/wireman/releases/download/v0.2.10/wireman-0.2.10-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "13c08423575f4fd2bf85202d496ea320607deac801eee729d76ada6de210ebf2"
     end
   end
   
