1c6e490c1121572a7caa0eef0959779cc654301d
Philipp Reiter <p.reiter93@gmail.com>
2025-12-27 10:42:23 +0100

    bump wireman version to v0.2.11

--- Formula/wireman.rb
+++ Formula/wireman.rb
@@ -1,27 +1,27 @@
 class Wireman < Formula
   desc "Wireman: A grpc client for the terminal"
   homepage "https://github.com/preiter93/wireman"
-  version "0.2.10"
+  version "0.2.11"
   license "MIT"
   head "https://github.com/preiter93/wireman.git", branch: "main"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/preiter93/wireman/releases/download/v0.2.10/wireman-0.2.10-aarch64-apple-darwin.tar.gz"
-      sha256 "57a3cd1d38b4822cba30222fec9b62fc51f097d7c732032367bf9e319c89e9b2"
+      url "https://github.com/preiter93/wireman/releases/download/v0.2.11/wireman-0.2.11-aarch64-apple-darwin.tar.gz"
+      sha256 "5f8422aeb18a95fff62fca93b1385dc3cf50df0c56e4be7856a93bf140a836df"
     else
-      url "https://github.com/preiter93/wireman/releases/download/v0.2.10/wireman-0.2.10-x86_64-apple-darwin.tar.gz"
-      sha256 "924dfdc8cb331c82b9b71967e873dd8436bd1eb1637d78cf48d416f032ba7925"
+      url "https://github.com/preiter93/wireman/releases/download/v0.2.11/wireman-0.2.11-x86_64-apple-darwin.tar.gz"
+      sha256 "b54b45560c0ab79b5b833bdc1e23313820cb5478d747df32ad9d208036033a7c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/preiter93/wireman/releases/download/v0.2.10/wireman-0.2.10-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "62f30b9190e1e811de4ea1c07fe872dfed8ece64048327dbf2edb2b3205cce4a"
+      url "https://github.com/preiter93/wireman/releases/download/v0.2.11/wireman-0.2.11-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "362e2962f6c1c42eb215460551d80002e2c20087fedc851a32adfb678cdf76d4"
     else
-      url "https://github.com/preiter93/wireman/releases/download/v0.2.10/wireman-0.2.10-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "13c08423575f4fd2bf85202d496ea320607deac801eee729d76ada6de210ebf2"
+      url "https://github.com/preiter93/wireman/releases/download/v0.2.11/wireman-0.2.11-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "19311880259677925bd453343f3653979f13730377bb4ed921444b4b52bfe0ca"
     end
   end
   
