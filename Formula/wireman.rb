857c1d0547af4eb6136f71a01b0df53220f3b7f2
Philipp Reiter <p.reiter93@gmail.com>
2026-01-13 22:50:33 +0100

    release v0.3.5

--- Formula/wireman.rb
+++ Formula/wireman.rb
@@ -1,27 +1,27 @@
 class Wireman < Formula
   desc "Wireman: A grpc client for the terminal"
   homepage "https://github.com/preiter93/wireman"
-  version "0.3.4"
+  version "0.3.5"
   license "MIT"
   head "https://github.com/preiter93/wireman.git", branch: "main"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/preiter93/wireman/releases/download/v0.3.4/wireman-0.3.4-aarch64-apple-darwin.tar.gz"
-      sha256 "34cbf03f1820d16de05e802ac61048e7ccf3d7c01910e633be88ba9165ea3141"
+      url "https://github.com/preiter93/wireman/releases/download/v0.3.5/wireman-0.3.5-aarch64-apple-darwin.tar.gz"
+      sha256 "7814244e57150c8bf290cab56094b056844e1c951615dab3ff48266ff2107b2c"
     else
-      url "https://github.com/preiter93/wireman/releases/download/v0.3.4/wireman-0.3.4-x86_64-apple-darwin.tar.gz"
-      sha256 "e0e5f0d5a6d309576499ddc67210061190b7fd26b133f34b26c0e74763473b5d"
+      url "https://github.com/preiter93/wireman/releases/download/v0.3.5/wireman-0.3.5-x86_64-apple-darwin.tar.gz"
+      sha256 "47c59fdfc67e076306e06747133b81f753a48e7bc0bdb834a41294b8d67718d8"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/preiter93/wireman/releases/download/v0.3.4/wireman-0.3.4-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "47c6caaa93cc1ec33fc433cbea3cbc230a1de95038b30c872f6e6e00e3669403"
+      url "https://github.com/preiter93/wireman/releases/download/v0.3.5/wireman-0.3.5-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "7c75c9b743175dd255c6baa2874245d342775fbdee8396ea567783db452337d1"
     else
-      url "https://github.com/preiter93/wireman/releases/download/v0.3.4/wireman-0.3.4-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "0135f18feaf0daf7e1d5c322e4428a331fb2b362f080c284c61b412162536fe0"
+      url "https://github.com/preiter93/wireman/releases/download/v0.3.5/wireman-0.3.5-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "a0bb5c2e7164febbf9cd7d079ecd9aaac3d60fc8c45167687a8bf00885990de1"
     end
   end
   
